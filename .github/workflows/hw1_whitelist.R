0342be6bb88b77f08b1f3ae119e14abec2a73ec6
sarahzeller <sarah.zeller@tu-dresden.de>
2025-07-14 16:52:21 +0200

    🐛 allow different .Rprojs

--- .github/workflows/hw1_whitelist.R
+++ .github/workflows/hw1_whitelist.R
@@ -1,18 +1,20 @@
-allowed_files = c("hw1.md",
+allowed_files <- c("hw1.md",
                   "hw1.Rmd",
+                  "hw1.qmd",
                   "README.md",
                   "hw1.Rproj",
                   "hw1_whitelist.R",
                   ".gitignore")
 
-files = dir()
-disallowed_files = files[!(files %in% allowed_files)]
+files <- dir()
+disallowed_files <- files[!(files %in% allowed_files)]
+disallowed_files_wo_Rproj <- disallowed_files[!grepl("\\.Rproj", disallowed_files)]
 
-if (length(disallowed_files != 0)) {
+if (length(disallowed_files_wo_Rproj != 0)) {
   cat("Disallowed files found:\n")
   cat("  (remove the following files from your repo)\n\n")
   
-  for (file in disallowed_files)
+  for (file in disallowed_files_wo_Rproj)
     cat("*", file, "\n")
   
   quit("no", 1, FALSE)
